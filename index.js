eae23e1d63b027daff2dc50db6f4577fb28a6dcd
sagarvns <90026062+sagarvns@users.noreply.github.com>
2022-08-08 14:42:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("hellwo world")
+console.warn("hellwo warning")
+console.error("hellwo error")
+//do no write in repeat your self
+function avg(a,b){
+    return(a + b)/2;
+}
+c1=avg(4,6);
+
+var arr =[1,2,3,4,5]
+    for (var i=0; i<arr.length; i++);{
+        console.log(i);
+    }
+
+
+
+
+
+
+
+
